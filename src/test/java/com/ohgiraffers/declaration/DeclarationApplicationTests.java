3fe6e660469eae1fd380e4abfca35a9514939302
jeonghee <hongi_@naver.com>
2024-02-23 09:48:14 +0900

    first commit

---
+++ src/test/java/com/ohgiraffers/declaration/DeclarationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.declaration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeclarationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
